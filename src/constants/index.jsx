7dda34003001a9a74de78bcc6dbf3dcba45d1f41
vaishnavisagarjadhav <135767546+vaishnavisagarjadhav@users.noreply.github.com>
2026-02-22 01:29:30 +0530

    Enhance event descriptions and add new events

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,11 +2,7 @@ export const events = [
     {
       heading: "Round 1: Marvel Mash-Up",
       date: "07-12-2024",
-      subHeading: `Get ready for a mind-bending, web-twisting challenge in Marvel Mash-Up!        Channel your inner Tony Stark and solve puzzles to unlock Marvel’s mysteries.
-      This isn’t just riddles – you’ll dive into Web Development!
-      Think of it as superpower training. Test your coding skills and Marvel knowledge.
-      Only sharp minds can assemble clues to unlock the next level.
-      Can you outwit Loki’s tricks and outcode the Avengers? The Multiverse’s fate is in your hands!`,
+      subHeading: `Get ready for a mind-bending, web-twisting challenge in Marvel Mash-Up! Channel your inner Tony Stark and solve puzzles to unlock Marvel’s mysteries. This isn’t just riddles – you’ll dive into Web Development! Think of it as superpower training. Test your coding skills and Marvel knowledge as you face a series of interactive coding puzzles, Marvel-themed logic games, and creative problem-solving tasks. Only sharp minds can assemble clues to unlock the next level. Can you outwit Loki’s tricks and outcode the Avengers? The Multiverse’s fate is in your hands! Each puzzle you solve brings you closer to becoming a true Marvel hero, and every challenge is designed to push your limits and ignite your imagination. Are you ready to assemble and prove your worth?`,
       direction: "right",
       superhero: "/superheroes/ironman.png",
       scale:1,
@@ -15,7 +11,7 @@ export const events = [
     {
       heading: "Round 2: Quest for the Next Nexus",
       date:"08-12-2024 to 14-12-2024",
-      subHeading:`Gather your team for a thrilling race against time in the Quest for the Next Nexus! Think of it as your very own Infinity Stone scavenger hunt filled with riddles and puzzles designed to test your problem-solving skills. Each riddle brings you one step closer to unlocking the secrets of the multiverse. But beware – the clock is ticking, and only the sharpest minds can prevail. Will you rise to the challenge like Captain Marvel or fall into Doctor Strange’s time loop?`,
+      subHeading:`Gather your team for a thrilling race against time in the Quest for the Next Nexus! Think of it as your very own Infinity Stone scavenger hunt filled with riddles, puzzles, and cryptic clues designed to test your problem-solving skills and teamwork. Each riddle brings you one step closer to unlocking the secrets of the multiverse, with every solved puzzle revealing a new layer of the Marvel Universe. But beware – the clock is ticking, and only the sharpest minds can prevail. Will you rise to the challenge like Captain Marvel or fall into Doctor Strange’s time loop? Collaborate, strategize, and race against other teams to prove you have what it takes to be the next Nexus champion!`,
       direction: "left",
       superhero: "/superheroes/captainmarvel.png",
       scale:1.28,
@@ -24,7 +20,7 @@ export const events = [
     {
       heading: "Round 3 : The Realm of Asgard",
       date:"15-12-2024",
-      subHeading:`Welcome to the Realm of Asgard, where only the bravest dare to enter! In this virtual realm, hidden objects and treasures await discovery. Channel your inner Thor – you’ll need more than just a mighty hammer to solve these intricate puzzles. Your detective skills will be tested, but beware: Loki’s mischief hides around every corner. Only those with sharp eyes (or divine wisdom) will prevail. Will you uncover Asgard’s secrets or succumb to its deceptive traps?`,
+      subHeading:`Welcome to the Realm of Asgard, where only the bravest dare to enter! In this virtual realm, hidden objects and treasures await discovery. Channel your inner Thor – you’ll need more than just a mighty hammer to solve these intricate puzzles. Your detective skills will be tested as you search for clues, decipher ancient runes, and unlock the secrets of the gods. But beware: Loki’s mischief hides around every corner, and only those with sharp eyes (or divine wisdom) will prevail. Will you uncover Asgard’s secrets or succumb to its deceptive traps? Every discovery brings you closer to the throne of Asgard, but only the most resourceful will claim victory!`,
       direction: "right",
       superhero: "/superheroes/thor.png",
       scale:1.26,
@@ -33,7 +29,7 @@ export const events = [
     {
         heading: "Round 4: Return of the Avengers",
         date:"16-12-2024",
-        subHeading:`The Return of the Avengers is here! After conquering Asgard and solving Nexus codes, it’s time for the ultimate showdown. Assemble your team for the final evaluation, where you’ll prove your skills in an epic challenge. The clock is ticking – this is your chance to shine. Showcase everything you’ve learned as we crown the champions of the Marvel Universe. The grand finale awaits – your moment of glory and triumph in this legendary journey!`,
+        subHeading:`The Return of the Avengers is here! After conquering Asgard and solving Nexus codes, it’s time for the ultimate showdown. Assemble your team for the final evaluation, where you’ll prove your skills in an epic challenge that combines everything you’ve learned so far. The clock is ticking – this is your chance to shine. Showcase your coding prowess, teamwork, and Marvel knowledge as we crown the champions of the Marvel Universe. The grand finale awaits – your moment of glory and triumph in this legendary journey! Expect surprise twists, bonus rounds, and a test of true heroism as you battle for the title of Ultimate Avenger!`,
         direction: "left",
         superhero: "/superheroes/avengers2.png",
         scale:2.05,
@@ -43,36 +39,90 @@ export const events = [
 
   export const events1 = [
     {
-      title: "Game1",
-      date: "Date",
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      title: "Prompt to Image Challenge",
+      date: "28 Feb 2026",
+      content: `Recreate a given AI-generated reference image using only text prompts. Teams will have just 2 minutes to craft the perfect prompt using Google Gemini. The closer your generated image matches the reference, the higher your score. Creativity meets precision. Can your words paint the perfect picture? This challenge will test your ability to communicate visually through language, pushing your imagination and descriptive skills to the limit. Think outside the box and let your creativity shine as you compete for the top spot!`,
       // direction: "right",
       superhero: "/favicon.ico",
       scale:0.5,
       position:113
     },
     {
-      title: "Game 2",
-      date:"Date",
-      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      title: "UI Disaster",
+      date:"28 Feb 2026",
+      content:`Redesign an intentionally messy and confusing user interface. Teams will have 15 minutes to transform it into a clean and user-friendly wireframe. You must not only redesign it, but also explain the logic behind your UX decisions. Design is not just how it looks — it’s how it works. Present your wireframe and walk the judges through your thought process, highlighting usability improvements, accessibility features, and creative solutions. Show us how you turn chaos into clarity!`,
       // direction: "left",
       superhero: "/favicon.ico",
       scale:0.6,
       position:164
     },
     {
-      title: "game 3",
-      date:"date",
-      content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      title: "Fast Typing",
+      date:"28 Feb 2026",
+      content:`Test your speed and accuracy in a 45-second typing sprint. The participant with the highest correct word count wins. Stay focused. Every second counts. Compete head-to-head with others in a fast-paced challenge that rewards both quick thinking and precision. Will your fingers fly across the keyboard, or will nerves get the best of you? Only the fastest and most accurate will claim victory!`,
       // direction: "right",
       superhero: "/favicon.ico",
       scale:0.6,
       position:150
     },
     {
-        title: "Game 4",
-        date:"date",
-        content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        title: "Guess the Output",
+        date:"28 Feb 2026",
+        content:`A short code snippet will be displayed on the screen. Teams must analyze it and predict the correct output within 2 minutes. Think fast. One small detail can change everything. This challenge will test your programming knowledge, attention to detail, and ability to reason under pressure. Discuss with your team, break down the logic, and submit your answer before time runs out!`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Reverse Engineering",
+        date:"28 Feb 2026",
+        content:`You will be given a final output or result. Your task is to work backward and write the logic or code that could have produced it. Decode the solution. Prove your logical thinking. This round is all about reverse engineering and creative problem-solving. Can you unravel the mystery and reconstruct the process step by step? Show us your analytical skills and coding expertise!`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Explain the Meme",
+        date:"28 Feb 2026",
+        content:`A viral tech or non-tech meme will be shown. Teams must explain its meaning, context, and hidden humor. It’s not just funny — it’s about understanding the idea behind it. Dive deep into the world of memes, decode the references, and impress the judges with your wit and insight. This round rewards those who can see beyond the surface and appreciate the layers of humor and culture!`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Logo Remix",
+        date:"28 Feb 2026",
+        content:`A modified version of a well-known brand logo will appear on screen. Colors or shapes may be changed. Identify the original brand within 15 seconds. Sharp eyes win. This challenge is all about visual memory and brand awareness. Can you spot the subtle differences and recall the original logo before time runs out? Only the most observant will succeed!`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Arcade Dart Game",
+        date:"28 Feb 2026",
+        content:`Step up and aim for the target. Players throw darts to score maximum points. Focus. Aim. Hit. Precision decides the winner. Test your hand-eye coordination and nerves under pressure as you compete for the highest score. Every throw counts, and only the steadiest hands will emerge victorious!`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Bottle Flip Challenge",
+        date:"28 Feb 2026",
+        content:`Participants must land the bottle upright within 10 attempts. Control and consistency matter more than luck. One perfect flip can change the game. This fun and challenging round will test your patience, skill, and determination. Will you master the art of the bottle flip and claim bragging rights?`,
+        // direction: "left",
+        superhero: "/favicon.ico",
+        scale:0.8,
+        position:200
+      },
+    {
+        title: "Build Cup Pyramid",
+        date:"28 Feb 2026",
+        content:`You get only one attempt to build a perfect cup pyramid. Balance, coordination, and steady hands are key. One chance. Make it count. This round is all about focus and finesse. Can you build the tallest, most stable pyramid in a single try? The pressure is on!`,
         // direction: "left",
         superhero: "/favicon.ico",
         scale:0.8,
@@ -84,25 +134,25 @@ export const events = [
     {
       title: "Technical Support",
       details: [
-        "loremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        "Our dedicated technical support team is available throughout the event to assist you with any issues, questions, or technical difficulties you may encounter. Whether you need help accessing resources, troubleshooting a problem, or understanding event rules, our experts are here to ensure your experience is smooth and enjoyable. Don’t hesitate to reach out for guidance or support at any stage of the competition!",
       ],
     },
     {
       title: "Live Updates and Leaderboards",
       details: [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        "Stay up-to-date with real-time updates and live leaderboards! Track your team’s progress, see how you stack up against the competition, and celebrate every milestone. Our live updates ensure you never miss an important announcement, schedule change, or exciting development. The leaderboard is updated instantly, so you’ll always know who’s leading the race to victory!",
       ],
     },
     {
       title: "Team Size",
       details: [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        "Teams can consist of 2-4 members, allowing you to collaborate, brainstorm, and combine your unique strengths. Working together not only makes the challenges more fun but also increases your chances of success. Choose your teammates wisely, as teamwork and communication are key to overcoming obstacles and achieving greatness in the MatrixVerse!",
       ],
     },
     {
       title: "Participation Certificates",
       details: [
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+        "Every participant will receive a certificate recognizing their effort, creativity, and participation in the MatrixVerse event. These certificates are a testament to your hard work and dedication, and a great addition to your portfolio or resume. Celebrate your achievements and take home a memento of your journey through the Marvel-inspired challenges!",
       ],
     },
   ];
